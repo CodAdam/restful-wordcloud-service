b2e85151386d0e70fb85b6c9fde346a0a7f6c3d4
Kailin Yang <Kailin Yang>
2017-04-24 17:37:40 +0800

    chinese process

--- app.py
+++ app.py
@@ -3,16 +3,19 @@ from flask import Flask, jsonify
 from flask import abort
 from flask import make_response
 from flask import request
-
-#分词相关
 from os import path  
 from scipy.misc import imread  
 import matplotlib.pyplot as plt  
 import jieba  
+import json
+import urllib.parse
+
 from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator  
 
 app = Flask(__name__)
 
+global stopwords 
+        
 def importStopword(filename=''):  
     
     global stopwords  
@@ -28,7 +31,7 @@ def importStopword(filename=''):
     f.close()  
   
 def processChinese(text):  
-    seg_generator = jieba.cut(text)  # 使用结巴分词，也可以不使用  
+    seg_generator = jieba.cut(text)   
   
     seg_list = [i for i in seg_generator if i not in stopwords]  
   
@@ -39,54 +42,71 @@ def processChinese(text):
     return seg_list 
 
 
-@app.route('/getWordCloudPng/<string:text>', methods=['GET'])
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+@app.route('/getWordCloudPng/<string:wordtext>', methods=['POST','GET'])
 def get_word_cloud_png_by_text(wordtext):
     
-    stopwords = {}  
+    global stopwords
+    
+    stopwords={} 
     importStopword(filename='./stopwords.txt') 
-    # 获取当前文件路径  
-    # __file__ 为当前文件, 在ide中运行此行会报错,可改为  
-    # d = path.dirname('.')  
-    d = path.dirname(__file__)  
+    # __file__  
+    d = path.dirname('.')   
       
-    # 源码此处导入txt文本
+
     #text = open(path.join(d, 'love.txt'),encoding ='utf-8').read()  
       
-    # 项目需要改为服务传参
     text = wordtext
-      
-    #如果是中文  
-    text = processChinese(text) #中文不好分词，使用Jieba分词进行  
-      
+
+    text = processChinese(text)      
     # read the mask / color image  
     # taken from http://jirkavinse.deviantart.com/art/quot-Real-Life-quot-Alice-282261010  
-    # 设置背景图片  
-    back_coloring = imread(path.join(d, "./image/love.jpg"))  
+
+    back_coloring = imread(path.join(d, "./image/alice_color.png"))  
       
-    wc = WordCloud( font_path='./font/cabin-sketch.bold.ttf',#设置字体  
-                    background_color="black", #背景颜色  
-                    max_words=2000,# 词云显示的最大词数  
-                    mask=back_coloring,#设置背景图片  
-                    max_font_size=100, #字体最大值  
+    wc = WordCloud( #font_path=' C:/Users/Windows/fonts/msyh.ttf',
+                    #font_path='./font/cabin-sketch.bold.ttf',
+                    font_path='./font/鍙剁珛缇ゅ嚑浣曚綋.ttf', 
+                    background_color="white",  
+                    max_words=2000,  
+                    mask=back_coloring,  
+                    max_font_size=100,   
                     random_state=42,  
-                    )  
-    # 生成词云, 可以用generate输入全部文本(中文不好分词),也可以我们计算好词频后使用generate_from_frequencies函数  
+                    )
+      
+    
     wc.generate(text)  
-    # wc.generate_from_frequencies(txt_freq)  
-    # txt_freq例子为[('词a', 100),('词b', 90),('词c', 80)]  
-    # 从背景图片生成颜色值  
+    # wc.generate_from_frequencies(txt_freq)  
+
+
     image_colors = ImageColorGenerator(back_coloring)  
       
     plt.figure()  
-    # 以下代码显示图片  
-    plt.imshow(wc)  
-    plt.axis("off")  
-    plt.show()  
-    # 绘制词云  
+  
+    #plt.imshow(wc)  
+    #plt.axis("off")  
+    #plt.show()  
+ 
       
-    # 保存图片  
-    wc.to_file(path.join(d, "名称.png")) 
-    return jsonify({'result': "名称.png"})
+     
+    #wc.to_file(path.join(d, "name")) 
+    print((path.join(d, "./wordcloud/test.png")))
+    wc.to_file((path.join(d, "./wordcloud/test.png"))) 
+    host="http://127.0.0.1:8090/"
+    #dir="/wordcloud/"
+    dir = "wordcloud/"
+    name="test.png"
+    picurl=host+dir+name
+    return jsonify(
+                    {
+                            "code": "10000",
+                            "message": "鎴愬姛",
+                            "data": picurl
+                    })
+
 
 
 @app.errorhandler(404)
